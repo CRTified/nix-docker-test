d12cd5602099dc78982bb575e5959a19297e3840
Carl Richard Theodor Schneider <dev.github@crtified.me>
2023-02-20 23:25:59 +0100

    Somewhat working

--- flake.nix
+++ flake.nix
@@ -1,10 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
@@ -13,25 +10,49 @@
         nixosConfigurations.demoConfig = nixpkgs.lib.nixosSystem {
           inherit system;
           modules = [
+            # Required to boot without defining file systems
+            ({ config, pkgs, ... }: { boot.isContainer = true; })
+
+            # Demo Application
             ({ config, pkgs, ... }: {
+              users.users.root.password = "1234";
+
+              networking.firewall.enable = false;
               services.nginx = {
                 enable = true;
                 defaultHTTPListenPort = 3333;
                 virtualHosts.default = {
                   default = true;
                   root = "/";
+                  extraConfig = ''
+                    autoindex on;
+                  '';
                 };
               };
             })
           ];
         };
 
+        apps.default = {
+          type = "app";
+          program = toString (pkgs.writeShellScript "run-docker.sh" ''
+            docker run \
+              -ti \
+              --privileged \
+              -p 3333:3333 \
+              $(docker load < "${
+                self.packages."${system}".default
+              }" | grep -Po '[:a-z0-9]+$')
+          '');
+        };
+
         packages.default = pkgs.dockerTools.buildLayeredImage {
           name = "demoserver";
-          contents = [ self.nixosConfigurations.demoConfig.config.syste.build.toplevel ];
           config = {
             Cmd = [
-              "${self.nixosConfigurations.demoConfig.config.syste.build.toplevel}/init"
+              "${
+                self.nixosConfigurations."${system}".demoConfig.config.system.build.toplevel
+              }/init"
             ];
             ExposedPorts = { "3333/tcp" = { }; };
           };
